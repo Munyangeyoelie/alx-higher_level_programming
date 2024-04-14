8e7cc579bc618e179e9b7d15dca477c952d5cfd9
Munyangeyo Elie <munyangeyoelie38@gmail.com>
2024-04-14 11:53:51 -0700

    sha

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-"""Module that lists all states from the hbtn_0e_0_usa database."""
-import sys
+"""Lists states"""
+
 import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
-    # Get MySQL credentials from command-line arguments
-    # Connect to MySQL server
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-
-    # Execute the SQL query to retrieve all states sorted by id
-    c.execute("SELECT * FROM `states` ORDER BY `id`")
-    [print(state) for state in c.fetchall() if state[1][0] == "N"]
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if row[1].startswith("N"):
+            print(row)
+    cur.close()
+    conn.close()
