6ab34005657131847109dc57359f0d97aa433d33
Munyangeyo Elie <munyangeyoelie38@gmail.com>
2024-04-14 12:27:53 -0700

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -8,10 +8,9 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
-    query = """
-SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC"""
-    query = query.format(argv[4])
-    cur.execute(query)
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities "
+                "JOIN states ON cities.state_id = states.id "
+                "ORDER BY cities.id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
