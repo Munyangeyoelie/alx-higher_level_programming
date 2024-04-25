4a88898ced40b6334ff7a2e742e4e4c54e8a99d9
Munyangeyo Elie <munyangeyoelie38@gmail.com>
2024-04-25 07:07:30 -0700

    changes

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a GET request to a given URL with a header variable.
-curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
+# sends GET req with header var
+curl -s -H "X-School-User-Id:98" "$1"
